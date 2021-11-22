1653ecbd9dfe28cf5d839c6dcb96be74f406b5a6
Kiel Arneson <kiel.arneson@gmail.com>
2021-11-22 15:46:59 -0600

    Got web requests working to save action movie data and set action_movie_id on movies that include action in the 'genre' description, but I am not sure that it is the best way to proceed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,6 +25,7 @@ export default {
     return {
       searchQuery: "",
       movie: {},
+      actionMovie: { movie: {} },
     };
   },
   created: function () {},
@@ -35,6 +36,76 @@ export default {
         console.log("Show movie", response);
         this.movie = response.data;
         this.searchQuery = "";
+
+        if (this.movie.Genre.includes("Action") === true) {
+          var newActionMovieParams = {
+            title: this.movie.Title,
+            imdb_id: this.movie.imdbID,
+          };
+
+          axios.post("/action_movies", newActionMovieParams).then((response) => {
+            console.log("Create Action movie", response);
+            this.actionMovie = response.data;
+
+            var newMovieParams = {
+              title: this.movie.Title,
+              year: this.movie.Year,
+              rated: this.movie.Rated,
+              released: this.movie.Released,
+              runtime: this.movie.Runtime,
+              formatted_runtime: this.movie.formatted_runtime,
+              actors: this.movie.Actors,
+              awards: this.movie.Awards,
+              box_office: this.movie.BoxOffice,
+              director: this.movie.Director,
+              genre: this.movie.Genre,
+              language: this.movie.Language,
+              plot: this.movie.Plot,
+              poster: this.movie.Poster,
+              entertainment_type: this.movie.Type,
+              writer: this.movie.Writer,
+              imdb_id: this.movie.imdbID,
+              imdb_rating: this.movie.imdbRating,
+              imdb_votes: this.movie.imdbVotes,
+              internet_movie_database_rating: this.movie.Ratings[0].Value,
+              rotten_tomatoes_rating: this.movie.Ratings[1].Value,
+              metacritic_rating: this.movie.Ratings[2].Value,
+              action_movie_id: this.actionMovie.movie.id,
+            };
+            axios.post("/movies", newMovieParams).then((response) => {
+              console.log("Create movie", response);
+            });
+          });
+        } else {
+          var newMovieParams = {
+            title: this.movie.Title,
+            year: this.movie.Year,
+            rated: this.movie.Rated,
+            released: this.movie.Released,
+            runtime: this.movie.Runtime,
+            formatted_runtime: this.movie.formatted_runtime,
+            actors: this.movie.Actors,
+            awards: this.movie.Awards,
+            box_office: this.movie.BoxOffice,
+            director: this.movie.Director,
+            genre: this.movie.Genre,
+            language: this.movie.Language,
+            plot: this.movie.Plot,
+            poster: this.movie.Poster,
+            entertainment_type: this.movie.Type,
+            writer: this.movie.Writer,
+            imdb_id: this.movie.imdbID,
+            imdb_rating: this.movie.imdbRating,
+            imdb_votes: this.movie.imdbVotes,
+            internet_movie_database_rating: this.movie.Ratings[0].Value,
+            rotten_tomatoes_rating: this.movie.Ratings[1].Value,
+            metacritic_rating: this.movie.Ratings[2].Value,
+            action_movie_id: this.actionMovie.movie.id,
+          };
+          axios.post("/movies", newMovieParams).then((response) => {
+            console.log("Create movie", response);
+          });
+        }
       });
     },
   },
