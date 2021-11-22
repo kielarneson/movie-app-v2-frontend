e5b9e58a70b42cec18ff49b4c0712e12374337bb
Kiel Arneson <kiel.arneson@gmail.com>
2021-11-22 17:12:13 -0600

    Added comments to remind myself of where I am in terms of creating new movies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,6 +37,7 @@ export default {
         this.movie = response.data;
         this.searchQuery = "";
 
+        // This only creates a new movie if "Action" is included in the genre
         if (this.movie.Genre.includes("Action") === true) {
           var newActionMovieParams = {
             title: this.movie.Title,
@@ -75,35 +76,6 @@ export default {
             axios.post("/movies", newMovieParams).then((response) => {
               console.log("Create movie", response);
             });
-          });
-        } else {
-          var newMovieParams = {
-            title: this.movie.Title,
-            year: this.movie.Year,
-            rated: this.movie.Rated,
-            released: this.movie.Released,
-            runtime: this.movie.Runtime,
-            formatted_runtime: this.movie.formatted_runtime,
-            actors: this.movie.Actors,
-            awards: this.movie.Awards,
-            box_office: this.movie.BoxOffice,
-            director: this.movie.Director,
-            genre: this.movie.Genre,
-            language: this.movie.Language,
-            plot: this.movie.Plot,
-            poster: this.movie.Poster,
-            entertainment_type: this.movie.Type,
-            writer: this.movie.Writer,
-            imdb_id: this.movie.imdbID,
-            imdb_rating: this.movie.imdbRating,
-            imdb_votes: this.movie.imdbVotes,
-            internet_movie_database_rating: this.movie.Ratings[0].Value,
-            rotten_tomatoes_rating: this.movie.Ratings[1].Value,
-            metacritic_rating: this.movie.Ratings[2].Value,
-            action_movie_id: this.actionMovie.movie.id,
-          };
-          axios.post("/movies", newMovieParams).then((response) => {
-            console.log("Create movie", response);
           });
         }
       });
